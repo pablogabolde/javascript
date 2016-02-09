ce565cee28e16f63f7b817ea83a896b39fed466c
pablogabolde <pablo69580@gmail.com>
2016-02-09 11:42:19 +0100

    Affichage heure réussi

--- TP2GABOLDE_PETRINI_G1S4/javascript.js
+++ TP2GABOLDE_PETRINI_G1S4/javascript.js
@@ -1,12 +1,33 @@
 window.addEventListener("load", function (){
 
   
-
-  var date = new Date();
-
   var titreDate = document.getElementById("date");
 
-  titreDate.textContent = date;
+
+  function changerHeure(){
+    var date = new Date();
+    var heure = date.getHours();
+    var minute = date.getMinutes();
+    var seconde = date.getSeconds();
+    
+    //On rajoute un 0 devant si c'est inf à 10
+    if(heure < 10){
+      heure = "0" + heure;
+    }
+    if(minute < 10){
+      minute = "0" + minute;
+    }
+    if(seconde < 10){
+      seconde = "0" + seconde;
+    }
+
+    titreDate.textContent = heure + ":" + minute + ":" + seconde;
+
+    setTimeout(changerHeure, 1000);
+  }
+  
+  changerHeure();
+
 
 
 });
