d7e58e1d1446b74b5eb771e422b5d4c19773f0f1
pablogabolde <pablo69580@gmail.com>
2016-02-10 12:46:08 +0100

    Ajout de reveil : FONCTIONNEL

--- TP2GABOLDE_PETRINI_G1S4/javascript.js
+++ TP2GABOLDE_PETRINI_G1S4/javascript.js
@@ -2,48 +2,32 @@ window.addEventListener("load", function() {
 
     //variables
     var titreDate = document.getElementById("date");
-    var boutonAjouter = document.getElementById("boutonAjouter");
-    var tabTr = document.getElementsByTagName("tr");
-    var tailleTabTr = tabTr.length;
-    
 
     //Fonction pour regénérer l'heure
     function changerHeure(){
         var date = new Date();
-        var heure = date.getHours();
-        var minute = date.getMinutes();
-        var seconde = date.getSeconds();
-
-        //On rajoute un 0 devant si c'est inf à 10
-        if(heure < 10){
-          heure = "0" + heure;
-        }
-        if(minute < 10){
-          minute = "0" + minute;
-        }
-        if(seconde < 10){
-          seconde = "0" + seconde;
-        }
+        var heure = ('0'+date.getHours()).slice(-2);
+        var minute = ('0'+date.getMinutes()).slice(-2);
+        var seconde = ('0'+date.getSeconds()).slice(-2);
 
         titreDate.textContent = heure + ":" + minute + ":" + seconde;
-
         setTimeout(changerHeure, 1000);
     }
-  
     changerHeure();
-
     
-    // PROBLEME : Lorsque l'on ajoute une ligne, celle-ci disparait directement, de plus le clone marque  "	[object HTMLTableRowElement]" ...
-
+    
+    var boutonAjouter = document.getElementById("boutonAjouter");
     //Lorsque l'on clique sur le bouton
     boutonAjouter.addEventListener("click", function(){
         
-        //on récupère l'avant dernière ligne du tableau
-        var nouvelleAlarme = tabTr[0].cloneNode();
+        var modeleAlarme = document.getElementById("modeleAlarme");
+        var nouvelleAlarme = modeleAlarme.cloneNode(true);
 
-        //On l'insère avant le bouton ajouter
-        tabTr[tailleTabTr-1].insertAdjacentHTML("beforebegin", nouvelleAlarme);
+        //On l'insère à la fin de la liste des alarmes
+        //var listeAlarme = document.getElementById("listeAlarme");
+        modeleAlarme.parentNode.appendChild(nouvelleAlarme);
+
         
-    });
+    }); 
 
 });
