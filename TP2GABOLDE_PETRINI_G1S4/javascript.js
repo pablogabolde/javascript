7cb7916f9a10b2e3d0ad1ae1dc4e105aa575c443
pablogabolde <pablo69580@gmail.com>
2016-02-15 10:54:45 +0100

    Suppression OK

--- TP2GABOLDE_PETRINI_G1S4/javascript.js
+++ TP2GABOLDE_PETRINI_G1S4/javascript.js
@@ -2,7 +2,9 @@ window.addEventListener("load", function() {
 
     //variables
     var titreDate = document.getElementById("date");
-
+    //On récupère tous les boutons supprimés de la page
+    var listeAlarme = document.getElementById("listeAlarme");
+    
     //Fonction pour regénérer l'heure
     function changerHeure(){
         var date = new Date();
@@ -15,19 +17,34 @@ window.addEventListener("load", function() {
     }
     changerHeure();
     
-    
+    //Pour ajouter une alarme
     var boutonAjouter = document.getElementById("boutonAjouter");
-    //Lorsque l'on clique sur le bouton
     boutonAjouter.addEventListener("click", function(){
+        //var modeleAlarme = document.getElementById("modeleAlarme");
+        var modelesAlarme = document.getElementsByClassName("modeleAlarme");
+        var tailleTabAlarme = modelesAlarme.length - 1;
+        /*//On ajoute le bouton supprimé à l'alarme 1
+        var boutonSuppUn = modelesAlarme[0].getElementsByClassName("boutonSuppr");
+        boutonSuppUn[0].addEventListener("click", function(){
+            listeAlarme.removeChild(modelesAlarme[0]);
+        });*/
+              
+        var nouvelleAlarme = modelesAlarme[tailleTabAlarme].cloneNode(true);
+       
+        var boutonSupClone = nouvelleAlarme.getElementsByClassName("boutonSuppr");
+        //On ajoute l'évènement supprimé a la nouvelle alarme
+        boutonSupClone[0].addEventListener("click", function(){
+            listeAlarme.removeChild(nouvelleAlarme);
+        });
+  
         
-        var modeleAlarme = document.getElementById("modeleAlarme");
-        var nouvelleAlarme = modeleAlarme.cloneNode(true);
-
         //On l'insère à la fin de la liste des alarmes
-        //var listeAlarme = document.getElementById("listeAlarme");
-        modeleAlarme.parentNode.appendChild(nouvelleAlarme);
-
-        
+        listeAlarme.appendChild(nouvelleAlarme);    
     }); 
+    
+//    var liste = document.getElementById("liste");
+//    var audio = liste.getElementsByTagName("audio");
+//    console.log(audio);
+//    //audio.play();
 
 });
